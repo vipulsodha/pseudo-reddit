159c9c17c5dbff8a29a5f7ff23c08b3228c1099c
vipulsodha <vipulsodha@live.in>
2018-07-07 15:45:58 +0800

    Initialization function for server added

---
+++ app.js
@@ -0,0 +1,6 @@
+const Init = require('./app/index');
+
+/**
+ * Calling this function to start the server
+ */
+Init();
---
+++ app/index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by vipulsodha on 07/07/18.
+ */
+
+const Hapi = require('hapi');
+const serverConfig  = require('./config/serverConfig');
+const server = Hapi.server(serverConfig);
+
+
+/**
+ * Calling this function will initialize the web server.
+ * It will add all the respective routes, plugins, etc required and configured
+ */
+const Init = async () => {
+
+
+    await server.start();
+
+    console.log(`Server running at: ${server.info.uri}`);
+
+};
+
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+
+    process.exit(1);
+
+});
+
+
+
+module.exports = Init;
