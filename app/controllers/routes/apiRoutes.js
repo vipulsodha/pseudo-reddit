9fc02485ead3508e230f7caddcbb12508af4968c
vipulsodha <vipulsodha@live.in>
2018-07-07 15:53:52 +0800

    Util module for http methods added

--- app/controllers/routes/apiRoutes.js
+++ app/controllers/routes/apiRoutes.js
@@ -2,12 +2,13 @@
  * Created by vipulsodha on 07/07/18.
  */
 
-const configs = require('../routeConfigs/apiConfigs');
+const httpMethods = require('../../util/httpMethods');
+const configs = require('../routeConfigs/apiConfigs')
 
 module.exports =  [
-        {method: 'POST', path: '/api/topics', config: configs.createTopic},
-        {method: 'GET', path: '/api/topics', config: configs.getTopics},
-        {method: 'GET', path: '/api/topics/{topicId}', config: configs.getTopic},
-        {method: 'POST', path: '/api/topics/{topicId}/upvote', config: configs.upVoteTopic},
-        {method: 'POST', path: '/api/topics/{userId}/downvote', config: configs.downVoteTopic}
+        {method: httpMethods.POST, path: '/api/topics', config: configs.createTopic},
+        {method: httpMethods.GET, path: '/api/topics', config: configs.getTopics},
+        {method: httpMethods.GET, path: '/api/topics/{topicId}', config: configs.getTopic},
+        {method: httpMethods.POST, path: '/api/topics/{topicId}/upvote', config: configs.upVoteTopic},
+        {method: httpMethods.POST, path: '/api/topics/{userId}/downvote', config: configs.downVoteTopic}
 ];
