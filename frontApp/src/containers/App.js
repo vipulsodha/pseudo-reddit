a3977f187724b6d72ee47356090695f8af418295
vipulsodha <vipulsodha@live.in>
2018-07-07 21:03:18 +0800

    Front app directory structure created and Hello world created

---
+++ frontApp/src/containers/App.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vipulsodha on 07/07/18.
+ */
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+
+export default class App extends Component {
+
+    render() {
+        return <div>HelloWorld</div>;
+    }
+
+}
+
+
+
--- frontApp/src/index.js
+++ frontApp/src/index.js
@@ -1,6 +1,8 @@
 /**
  * Created by vipulsodha on 07/07/18.
  */
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './containers/App';
 
-module.exports = {};
-
+ReactDom.render(<App/>, document.getElementById("app"));
