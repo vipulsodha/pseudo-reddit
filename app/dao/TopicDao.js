2541fba1e4a8b88823ee5e0502df99a10c976654
vipulsodha <vipulsodha@live.in>
2018-07-08 04:10:42 +0800

    Basic dao interface done

---
+++ app/dao/TopicDao.js
@@ -0,0 +1,58 @@
+/**
+ * Created by vipulsodha on 07/07/18.
+ */
+
+const TreeDb = require('../db/TreeDb');
+const DB = TreeDb.InitDb();
+
+/**
+ * Inserts new topic in Db
+ * @param topic
+ * @return {Boolean}
+ */
+const insertNewTopic = (topic)  => {
+
+};
+
+/**
+ * gets the specific topic with topic ID
+ * @param topic
+ * @return {Object}
+ */
+const getTopic = (topicId)  => {
+
+};
+
+/**
+ * gets top N topics
+ * @param topic
+ * @return {Array.<Object>}
+ */
+const getTopNTopics = (topic)  => {
+
+};
+
+/**
+ * Deletes Topic with given topic Id if exists
+ * @param topicId
+ * @return {Boolean}
+ */
+const deleteTopic = (topicId)  => {
+
+};
+
+/**
+ * increments the upvote count for the given TopicId
+ * @param topicId
+ */
+const upVoteTopic = (topicId)  => {
+
+};
+
+/**
+ * increments the downvotes count for the given topidId
+ * @param topicId
+ */
+const downVoteTopic = (topicId)  => {
+
+};
