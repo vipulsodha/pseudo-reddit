1d8e78d0715c3037d8b48105bcbf6e76f1ad6cc1
vipulsodha <vipulsodha@live.in>
2018-07-08 16:48:01 +0800

    Db Node seperated

---
+++ app/db/DbNode.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vipulsodha on 08/07/18.
+ */
+
+
+const DbNode = function(title, author, upVotes, downVotes, createdTimeStamp, topicId) {
+
+    this.title = title;
+    this.author = author;
+    this.upVotes = upVotes;
+    this.downVotes = downVotes;
+    this.createdTimeStamp = createdTimeStamp;
+    this.topicId = topicId;
+    this.right = null;
+    this.left = null;
+};
+
+module.exports = DbNode;
