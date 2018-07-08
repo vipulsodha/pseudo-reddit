db2e0b77d3478800fa359f3ec11c70b25c17d7bd
vipulsodha <vipulsodha@live.in>
2018-07-08 16:50:05 +0800

    Dao integrated with Db

--- app/dao/TopicDao.js
+++ app/dao/TopicDao.js
@@ -2,57 +2,116 @@
  * Created by vipulsodha on 07/07/18.
  */
 
-const TreeDb = require('../db/TreeDb');
+const TreeDb  = require('../db/TreeDb');
+const Node = require('../db/DbNode');
+const Topic = require('../entities/Topic');
+
 const DB = TreeDb.InitDb();
 
+
 /**
- * Inserts new topic in Db
- * @param topic
- * @return {Boolean}
+ * @private
+ * @param {Topic} topic
+ */
+const mapTopicToDbNode = (topic) => {
+
+    return new Node(topic.title, topic.aurthor, topic.upVotes, topic.downVotes, topic.createdTimeStamp, topic.topicId);
+};
+
+/**
+ * @private
+ * @param {DbNode} node
+ */
+const mapDbNodeToTopic = (node) => {
+
+    return new Topic(node.title, node.aurthor, node.upVotes, node.downVotes, node.createdTimeStamp, node.topicId);
+};
+
+
+/**
+ * @public
+ * @param {Topic} topic
  */
 const insertNewTopic = (topic)  => {
 
+    DB.add(mapTopicToDbNode(topic))
 };
 
 /**
- * gets the specific topic with topic ID
- * @param topic
- * @return {Object}
+ * @public
+ * @param {int} topicId
+ * @return {Topic}
  */
 const getTopic = (topicId)  => {
 
+    DB.search(topicId);
 };
 
 /**
- * gets top N topics
- * @param topic
- * @return {Array.<Object>}
+ * @public
+ * @param {int} start
+ * @param {int} limit
+ * @return {Array.<Topic>}
  */
-const getTopNTopics = (topic)  => {
+const getTopics = (start = 1, limit = 20)  => {
 
+    const dbNodes = DB.getRangeItems(start, limit);
+
+    if (dbNodes === null) {
+        return null;
+    }
+
+    return dbNodes.map((node) => {
+       return mapDbNodeToTopic(node);
+    });
 };
 
 /**
- * Deletes Topic with given topic Id if exists
- * @param topicId
+ * @public
+ * @param {int} topicId
  * @return {Boolean}
  */
 const deleteTopic = (topicId)  => {
 
+    DB.delete(topicId);
+
+    return true;
 };
 
 /**
- * increments the upvote count for the given TopicId
- * @param topicId
+ * @public
+ * @param {int} topicId
+ * @return {Boolean}
  */
 const upVoteTopic = (topicId)  => {
 
+    DB.increaseUpVote(topicId);
+
+    return true;
 };
 
 /**
- * increments the downvotes count for the given topidId
- * @param topicId
+ * @public
+ * @param {int} topicId
+ * @return {Boolean}
  */
 const downVoteTopic = (topicId)  => {
 
+    DB.increaseDownVote(topicId);
+
+    return true;
+};
+
+const getTotalTopicCount = () => {
+  return DB.getCount();
+};
+
+module.exports = {
+    insertNewTopic,
+    getTopic,
+    getTopics,
+    deleteTopic,
+    upVoteTopic,
+    downVoteTopic,
+    getTotalTopicCount
 };
