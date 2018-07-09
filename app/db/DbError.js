09b7e1a6c76fab4e343fa76ca4c5050f59586272
vipulsodha <vipulsodha@live.in>
2018-07-10 03:09:04 +0800

    Db error constant file added

---
+++ app/db/DbError.js
@@ -0,0 +1,14 @@
+/**
+ * Created by vipulsodha on 10/07/18.
+ */
+
+
+module.exports = {
+    cannotAddAlreadyExists: {
+        message: "Cannot add record, it already exits with given key"
+    },
+    recordDoesNotExist: {
+        message: "Record does not exist"
+    }
+
+};
