23d0e8188074f251e73d749f6fdac2914f9f851b
vipulsodha <vipulsodha@live.in>
2018-07-10 04:19:08 +0800

    Static files served from nodejs server

--- app/config/serverConfig.js
+++ app/config/serverConfig.js
@@ -1,14 +1,17 @@
 /**
  * Created by vipulsodha on 07/07/18.
  */
-
+const Path = require('path');
 
 module.exports = (() => {
     return ({
         port: process.env.PORT || 3000,
         host: process.env.HOST ||'localhost',
         routes: {
-            cors: true
+            cors: true,
+            files: {
+                relativeTo: Path.join(__dirname, '../public')
+            }
         }
     })
 })();
--- app/controllers/routes/staticRoutes.js
+++ app/controllers/routes/staticRoutes.js
@@ -1,4 +1,28 @@
 /**
  * Created by vipulsodha on 07/07/18.
  */
-module.exports = [];
+
+/**
+ * This api contains all the static routes
+ * 
+ * @type {Array}
+ */
+
+const httpMethods = require('../../util/httpMethods');
+
+module.exports = [
+    {
+        method: httpMethods.GET,
+        path: '/',
+        handler: (request, h) => {
+            return h.file('index.html');
+        }
+    },
+    {
+        method: httpMethods.GET,
+        path: '/index.bundle.js',
+        handler: (request, h) => {
+            return h.file('index.bundle.js');
+        }
+    }
+];
