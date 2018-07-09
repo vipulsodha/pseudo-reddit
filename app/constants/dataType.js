d0145443c9afbb694e81a06d579656e85d26d50a
vipulsodha <vipulsodha@live.in>
2018-07-10 03:28:01 +0800

    Data types added for better data response and status code

---
+++ app/constants/dataType.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vipulsodha on 10/07/18.
+ */
+
+
+module.exports = {
+    DATA_CREATED : 'data_created',
+    DATA_UPDATED : 'data_updated',
+    DATA_DELETED : 'data_deleted',
+    DATA_RETRIEVED: 'data_retrieved'
+};
